a548c9137489a59709ba08c7e33768a1611faac1
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2024-12-15 14:05:14 +0530

    Time: 4 ms (5.05%), Space: 12.3 MB (5.63%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element-usingListComprehension.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        nums[:] = [x for x in nums if x!=val ]
+
+        print(len(nums))
+        print(nums)
+        
+        
+
