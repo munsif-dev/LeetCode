0ea3de371f3a9fee0835be16683fcc27ab758954
Ahamed Munsif <munsifshaifalikhan@gmail.com>
2025-12-16 13:43:07 +0530

    Time: 3 ms (95.49%), Space: 19.4 MB (35.93%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,18 +1,10 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        dup = missing = -1
-        mid_vec = [0] * (n+1)
-        for num in nums:
-            mid_vec[num] += 1
-
-        for i in range(1, len(mid_vec)):
-            if mid_vec[i] == 2:
-                dup = i
-            elif mid_vec[i] == 0:
-                missing = i
-        
-        return dup, missing
+        expected_sum = n * (n+1) /2
+        array_sum = sum(nums)
+        uniq_sum = sum(set(nums))
+        return int(array_sum - uniq_sum), int(expected_sum - uniq_sum)
 
 
 
