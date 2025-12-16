953b3aa2ca8c65e5f4943149b98d5b088aebe14c
Ahamed Munsif <munsifshaifalikhan@gmail.com>
2025-12-16 15:50:37 +0530

    Time: 15 ms (11.77%), Space: 14.3 MB (31.85%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+     // 1. create all number array
+        vector<int> all_num(101, 0);
         vector<int> out(nums.size());
-        for (int i = 0; i < nums.size(); i++){
-            int count = 0;
-            for (int j = 0; j < nums.size(); j++){
-                if (i != j and nums[j] < nums[i]){
-                    count++;
-                }
+        // 2. Find the occurance of each number in the array
+        for ( int i = 0; i < nums.size(); i++){
+            all_num[nums[i]]++;
+        }
+        
+        // 3. Creating a prefix array
+        for ( int i = 1; i < all_num.size(); i++){
+            all_num[i] += all_num[i-1];
+        }
+        // inspecting the all_nums array
+        for (int i : all_num){
+            cout << i << " ";
+        }
+        // 4. returning the output array
+        for (int i = 0; i < nums.size() ; i++){
+            if (nums[i] == 0){
+            out[i] = 0;
             }
-            out[i] = count;
+            else{
+                out[i] = all_num[nums[i]-1];
+            }
         }
         return out;
-    }
+    }    
 };
