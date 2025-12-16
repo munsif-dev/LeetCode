74261eb84d4da1ec1c7d4e16e720be4e2976a912
Ahamed Munsif <munsifshaifalikhan@gmail.com>
2025-12-16 13:38:27 +0530

    Time: 56 ms (10.59%), Space: 36.8 MB (6.79%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,15 +3,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> mid_vec(nums.size()+1, 0);
-        int dup, missing = -1;
-        for (int i = 0; i < nums.size(); i++){
-            mid_vec[nums[i]]++;
+        int n = nums.size();
+        int expected_sum = n * (n+1)/2;
+        int array_sum = 0;
+        int uniq_sum = 0;
+        for (int i = 0; i < n ; i ++){
+            array_sum += nums[i];
         }
-        for (int i = 1 ; i < mid_vec.size(); i++){
-            if (mid_vec[i] == 2) dup = i;
-            else if (mid_vec[i] == 0) missing = i;
+        set<int> uniq(nums.begin(),nums.end());
+        for (int num : uniq){
+            uniq_sum += num;
         }
-        return {dup, missing};
+
+        return {array_sum-uniq_sum, expected_sum-uniq_sum};
+
     }
 };
