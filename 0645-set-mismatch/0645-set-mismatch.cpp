d463b298b75d86d2c550bc8affb102001688673e
Ahamed Munsif <munsifshaifalikhan@gmail.com>
2025-12-16 08:16:41 +0530

    Time: 0 ms (100%), Space: 26.3 MB (53.25%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,24 +1,16 @@
+#include <iostream>
+using namespace std;
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int dup = -1;
-        int missing = -1;
-
-        for(int i = 1; i <= nums.size(); i++)
-        {
-            int count = 0;
-            for (int j = 0; j < nums.size(); j++)
-            {   
-                if (nums[j] == i)
-                {
-                    count++;
-                }
-            }
-
-            if (count == 2)
-                dup = i;
-            if (count == 0)
-                missing = i;
+        vector<int> mid_vec(nums.size()+1, 0);
+        int dup, missing = -1;
+        for (int i = 0; i < nums.size(); i++){
+            mid_vec[nums[i]]++;
+        }
+        for (int i = 1 ; i < mid_vec.size(); i++){
+            if (mid_vec[i] == 2) dup = i;
+            else if (mid_vec[i] == 0) missing = i;
         }
         return {dup, missing};
     }
